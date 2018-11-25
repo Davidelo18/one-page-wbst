18ba33fa5f0f1b1f1f6a1495e69bea9d97765d4c
davidelo <daw.kielbik@gmail.com>
2018-11-25 16:04:36 +0100

    created a banner and typing animation to headers

---
+++ scripts/typing.js
@@ -0,0 +1,30 @@
+// functions to do after loading the DOM
+document.addEventListener('DOMContentLoaded', function () {
+    typeWriter();
+});
+
+/* --- DECLARED FUNCTIONS AREA --- */
+
+// typing animation the 'welcome' word
+const txtElements = document.querySelectorAll('.banner hgroup h1');
+const words = ['Welcome!', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'];
+let currentPosition = 0;
+let myIndex = 0;
+const delayTime = [300, 90];
+
+function typeWriter() {
+    let visibleWord = words[myIndex].substring(0, currentPosition);
+    txtElements[myIndex].textContent = visibleWord;
+    if (currentPosition < words[myIndex].length) {
+        currentPosition++;
+        setTimeout("typeWriter()", delayTime[myIndex]);
+    } else {
+        txtElements[myIndex].classList.remove('typing');
+        if(myIndex < words.length){
+            myIndex++;
+            txtElements[myIndex].classList.add('typing');
+            currentPosition = 0;
+            setTimeout("typeWriter()", 500);
+        }
+    }
+}
